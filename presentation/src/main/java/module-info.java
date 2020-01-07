ec8d81233660dfb352ccf1aafba443eb82aa5149
dtrilsbeek <d.trilsbeek@gmail.com>
2020-01-07 10:15:20 +0100

    updated jackson

--- presentation/src/main/java/module-info.java
+++ presentation/src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module presentation {
     requires io.javalin;
     requires j2html;
-    requires jackson.databind;
-    requires jackson.annotations;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     requires org.eclipse.jetty.websocket.api;
     requires logic;
 
-    opens presentation.models to jackson.databind;
-    opens presentation.models.messages to jackson.databind;
+    opens presentation.models to com.fasterxml.jackson.databind;
+    opens presentation.models.messages to com.fasterxml.jackson.databind;
 }
