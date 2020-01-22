6fc286bf8f4015fd4fe484a586061cca6409b24f
dtrilsbeek <d.trilsbeek@gmail.com>
2020-01-22 11:13:16 +0100

    updated tests

--- logic/src/test/java/RouteFinderSpeedGridTest.java
+++ logic/src/test/java/RouteFinderSpeedGridTest.java
@@ -1,135 +1,104 @@
 import org.junit.jupiter.api.Test;
-import route.ExampleMapReal;
 import route.ExampleMapSquare;
 import route.RouteFinder;
 import route.RouteMap;
 import route.interfaces.IRouteMap;
 import route.interfaces.IRouteMapExample;
+import route.model.Intersection;
 import route.util.TimeStamp;
 
-import java.security.Timestamp;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RouteFinderSpeedGridTest {
 
+    private boolean isEnabled = false;
     private IRouteMap routeMap = new RouteMap(800, 800, 20);
     private IRouteMapExample routeMapExample;
     private TimeStamp timeStamp = new TimeStamp();
+
+    private Map<Integer, Intersection> createAndTestSpeed(int columnWidth) {
+
+        if (!isEnabled) {
+            columnWidth = 10;
+        }
+        routeMapExample = new ExampleMapSquare(routeMap, columnWidth);
+
+        var from = routeMap.getIntersection(0);
+        var to = routeMap.getIntersection(columnWidth * columnWidth -1);
+
+        System.out.println("start");
+        timeStamp.setBegin("Start Finding Route, GridWidth: "+columnWidth);
+        //Act
+        var routeFinder = new RouteFinder(routeMap, from, to);
+
+        //Assert
+        var route = routeFinder.getFinalRoute();
+        timeStamp.setEnd("Route Found! Route Length: "+route.size());
+        System.out.println(timeStamp);
+
+        return route;
+    }
 
     @Test
     void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_10()
     {
         //Arrange
         int columnWidth = 10;
-        routeMapExample = new ExampleMapSquare(routeMap, columnWidth);
 
-        var from = routeMap.getIntersection(0);
-        var to = routeMap.getIntersection(columnWidth * columnWidth -1);
-
-        System.out.println("start");
-        timeStamp.setBegin("Start Finding Route, GridWidth: "+columnWidth);
         //Act
-        var routeFinder = new RouteFinder(routeMap, from, to);
+        var route = createAndTestSpeed(columnWidth);
 
         //Assert
-        var route = routeFinder.getFinalRoute();
-        timeStamp.setEnd("Route Found! Route Length: "+route.size());
-        System.out.println(timeStamp);
-
         assertFalse(route.isEmpty());
     }
 
-/*    @Test
+    @Test
     void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_100()
     {
         //Arrange
         int columnWidth = 100;
-        routeMapExample = new ExampleMapSquare(routeMap, columnWidth);
-
-        var from = routeMap.getIntersection(0);
-        var to = routeMap.getIntersection(columnWidth * columnWidth -1);
-
-        System.out.println("Start");
-        timeStamp.setBegin("Finding Route, GridWidth: "+columnWidth);
         //Act
-        var routeFinder = new RouteFinder(routeMap, from, to);
+        var route = createAndTestSpeed(columnWidth);
 
         //Assert
-        var route = routeFinder.getFinalRoute();
-        timeStamp.setEnd("Route Found! Route Length: "+route.size());
-        System.out.println(timeStamp);
-
-        assertFalse(route.isEmpty());
-    }
+        assertFalse(route.isEmpty());    }
 
     @Test
     void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_1000()
     {
         //Arrange
         int columnWidth = 1000;
-        routeMapExample = new ExampleMapSquare(routeMap, columnWidth);
-
-        var from = routeMap.getIntersection(0);
-        var to = routeMap.getIntersection(columnWidth * columnWidth -1);
-
-        System.out.println("start");
-        timeStamp.setBegin("Start Finding Route, GridWidth: "+columnWidth);
         //Act
-        var routeFinder = new RouteFinder(routeMap, from, to);
+        var route = createAndTestSpeed(columnWidth);
 
         //Assert
-        var route = routeFinder.getFinalRoute();
-        timeStamp.setEnd("Route Found! Route Length: "+route.size());
-        System.out.println(timeStamp);
-
         assertFalse(route.isEmpty());
     }
 
     @Test
-    void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_10_000()
+    void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_1500()
     {
         //Arrange
         int columnWidth = 1500;
-        routeMapExample = new ExampleMapSquare(routeMap, columnWidth);
-
-        var from = routeMap.getIntersection(0);
-        var to = routeMap.getIntersection(columnWidth * columnWidth -1);
-
-        System.out.println("start");
-        timeStamp.setBegin("Start Finding Route, GridWidth: "+columnWidth);
         //Act
-        var routeFinder = new RouteFinder(routeMap, from, to);
+        var route = createAndTestSpeed(columnWidth);
 
         //Assert
-        var route = routeFinder.getFinalRoute();
-        timeStamp.setEnd("Route Found! Route Length: "+route.size());
-        System.out.println(timeStamp);
-
         assertFalse(route.isEmpty());
     }
 
     @Test
-    void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_100_000()
+    void shouldFindRoute_BetweenStartAndDest_ExampleMapSquare_3000()
     {
         //Arrange
         int columnWidth = 3000;
-        routeMapExample = new ExampleMapSquare(routeMap, columnWidth);
-
-        var from = routeMap.getIntersection(0);
-        var to = routeMap.getIntersection(columnWidth * columnWidth -1);
-
-        System.out.println("Start. IntersectionAmount: "+ columnWidth * columnWidth);
-        timeStamp.setBegin("Start Finding Route, GridWidth: "+columnWidth);
         //Act
-        var routeFinder = new RouteFinder(routeMap, from, to);
+        var route = createAndTestSpeed(columnWidth);
 
         //Assert
-        var route = routeFinder.getFinalRoute();
-        timeStamp.setEnd("Route Found! Route Length: "+route.size());
-        System.out.println(timeStamp);
-
         assertFalse(route.isEmpty());
-    }*/
+    }
 }
--- logic/src/test/java/RouteFinderTest.java
+++ logic/src/test/java/RouteFinderTest.java
@@ -31,16 +31,33 @@ public class RouteFinderTest {
 
     @Test
     void shouldReturnExplored_WhenRoute_isFound() {
+        //Arrange
+        routeMap = new ExampleMapReal(routeMap).getRouteMap();
+        var from = routeMap.getIntersection(29);
+        var to = routeMap.getIntersection(48);
 
+        //Act
+        var routeFinder = new RouteFinder(routeMap, from, to);
+
+        //Assert
+        var route = routeFinder.getExplored();
+        assertFalse(route.isEmpty());
     }
 
     @Test
     void shouldReturnEmpty_When_StartIsSameAsDest() {
+        //Arrange
+        routeMap = new ExampleMapReal(routeMap).getRouteMap();
+        var from = routeMap.getIntersection(29);
+        var to = routeMap.getIntersection(29);
 
-    }
+        //Act
+        var routeFinder = new RouteFinder(routeMap, from, to);
 
-    @Test
-    void shouldReturnFastestRoute_FromExplored_WhenRouteIsFound() {
+        //Assert
+        var route = routeFinder.getFinalRoute();
+        var result = route.size() < 2;
 
+        assertTrue(result);
     }
 }
